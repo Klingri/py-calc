834c90ada0f232796a7e7ff3eb078e496aded7e0
Klingri <klingri52@gmail.com>
2025-06-10 15:43:14 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,3 +1,24 @@
-X=5
-Y=8
-print(X+Y, X-Y, X/Y)
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+print("Please select operation -\n"
+      "1. Add\n"
+      "2. Subtract\n"
+      "3. Multiply\n"
+      "4. Divide\n")
+
+sel = int(input("Select operation (1-4): "))
+
+# test
